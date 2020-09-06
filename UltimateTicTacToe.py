2da4a3254a1e478ab59bfab266d1cb6d1b7ad258
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2020-09-06 19:30:29 -0400

    Complete draw X

--- UltimateTicTacToe.py
+++ UltimateTicTacToe.py
@@ -2,14 +2,11 @@ from typing import List
 from UserInput import *
 from BoardUI import *
 
-COORDINATE_ADJUSTMENTS = {0: (0, 0), 1: (0, 4), 2: (0, 8), 3: (4, 0), 4: (4, 4), 5: (4, 8), 6: (8, 0), 7: (8, 4), 8: (8, 8)}
-SPACE_INDEX_TO_COORDINATE = {0: (0, 0), 1: (0, 1), 2: (0, 2), 3: (1, 0), 4: (1, 1), 5: (1, 2), 6: (2, 0), 7: (2, 1), 8: (2, 2)}
+COORDINATE_ADJUSTMENTS = {0: (0, 0), 1: (0, 8), 2: (0, 16), 3: (8, 0), 4: (8, 8), 5: (8, 16), 6: (16, 0), 7: (16, 8), 8: (16, 16)}
+SPACE_INDEX_TO_COORDINATE = {0: (0, 0), 1: (0, 2), 2: (0, 4), 3: (2, 0), 4: (2, 2), 5: (2, 4), 6: (4, 0), 7: (4, 2), 8: (4, 4)}
 
 EMPTY_TIC_TAC_TOE_BOARD = [0] * 9
 EMPTY_ULTIMATE_TIC_TAC_TOE_BOARD = [EMPTY_TIC_TAC_TOE_BOARD for _ in range(9)]
-# BLANK_ROW = ["x", "|", "x", "|", "x", " ", "|", " ", "x", "|", "x", "|", "x", " ", "|", " ", "x", "|", "x", "|", "x"]
-# BLANK_ROW = [" ", "|", " ", "|", " ", " ", "|", " ", " ", "|", " ", "|", " ", " ", "|", " ", " ", "|", " ", "|", " "]
-# BLANK_UI = [BLANK_ROW for _ in range(9)]
 
 P1_MOVE_VALUE = 1
 P2_MOVE_VALUE = 4
@@ -42,28 +39,8 @@ def initializeEmptyBoard():
     rowWithSpaces = [i for i in "      |       |      "]
     borderRow = [i for i in "------|-------|------"]
 
-    return [
-        rowWithPipes, 
-        rowWithDashes, 
-        rowWithPipes, 
-        rowWithDashes, 
-        rowWithPipes, 
-        rowWithSpaces, 
-        borderRow, 
-        rowWithPipes, 
-        rowWithDashes, 
-        rowWithPipes, 
-        rowWithDashes, 
-        rowWithPipes, 
-        rowWithSpaces, 
-        borderRow, 
-        rowWithPipes, 
-        rowWithDashes, 
-        rowWithPipes, 
-        rowWithDashes, 
-        rowWithPipes, 
-        rowWithSpaces, 
-    ]
+    # I alter the contents of rowWithPipes and rowWithDashes to show where user played on the screen. However, if I didn’t use copy() then each rowWithPipes in my 2D list would reference the exact same list in memory (same for rowWithDashes), thereby changing each row when I only want to change one row. This seems like an inelegant solution though...
+    return [rowWithPipes.copy(), rowWithDashes.copy(), rowWithPipes.copy(), rowWithDashes.copy(), rowWithPipes.copy(), rowWithSpaces, borderRow, rowWithSpaces, rowWithPipes.copy(), rowWithDashes.copy(), rowWithPipes.copy(), rowWithDashes.copy(), rowWithPipes.copy(), rowWithSpaces, borderRow, rowWithSpaces, rowWithPipes.copy(), rowWithDashes.copy(), rowWithPipes.copy(), rowWithDashes.copy(), rowWithPipes.copy(), rowWithSpaces]
 
 
 def clearSpaceOnBigBoard(uiRows: List[List[str]], rowOffset: int, columnOffset: int):
@@ -73,14 +50,14 @@ def clearSpaceOnBigBoard(uiRows: List[List[str]], rowOffset: int, columnOffset: 
 
 def initializeXBoard(uiRows: List[List[str]], rowOffset: int, columnOffset: int):
     clearSpaceOnBigBoard(uiRows, rowOffset, columnOffset)
-    # for r in range(0, 5):
-    #     for c in range(0, 5):
-    #         if r != c: continue
-    #         uiRows[r + rowOffset][c + columnOffset] = "x"
-    # uiRows[0 + rowOffset][4 + columnOffset] = "x"
-    # uiRows[4 + rowOffset][0 + columnOffset] = "x"
-    # uiRows[1 + rowOffset][3 + columnOffset] = "x"
-    # uiRows[3 + rowOffset][1 + columnOffset] = "x"
+    for r in range(0, 5):
+        for c in range(0, 5):
+            if r != c: continue
+            uiRows[r + rowOffset][c + columnOffset] = "x"
+    uiRows[0 + rowOffset][4 + columnOffset] = "x"
+    uiRows[4 + rowOffset][0 + columnOffset] = "x"
+    uiRows[1 + rowOffset][3 + columnOffset] = "x"
+    uiRows[3 + rowOffset][1 + columnOffset] = "x"
 
 
 
