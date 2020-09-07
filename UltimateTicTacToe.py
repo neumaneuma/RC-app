0fba2193d39b34116f47862b80f0764460ac9956
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2020-09-07 12:32:19 -0400

    Finish perform move methods

--- UltimateTicTacToe.py
+++ UltimateTicTacToe.py
@@ -2,15 +2,15 @@ from UserInput import *
 from BoardUI import *
 
 EMPTY_TIC_TAC_TOE_BOARD = [0] * 9
-EMPTY_ULTIMATE_TIC_TAC_TOE_BOARD = [EMPTY_TIC_TAC_TOE_BOARD for _ in range(9)]
+EMPTY_ULTIMATE_TIC_TAC_TOE_BOARD = [[0] * 9 for _ in range(9)]
 
 P1_MOVE_VALUE = 1
 P2_MOVE_VALUE = 4
 
 class Board:
-    def __init__(self, bigBoard, miniBoards):
-        self.bigBoard = bigBoard
-        self.miniBoards = miniBoards
+    def __init__(self):
+        self.bigBoard = EMPTY_TIC_TAC_TOE_BOARD
+        self.miniBoards = EMPTY_ULTIMATE_TIC_TAC_TOE_BOARD
     
 class Player:
     def __init__(self, draw, name, character, isP1):
@@ -26,29 +26,47 @@ def playGame(board: Board, p1: Player, p2: Player):
     userSelectsSpaceOnBigBoard = True
     currentPlayer = p1
 
-# def updateBoard(board: Board, p1: Player, p2: Player):
+# These methods may seem like they're doing too much based on the number of parameters they have, but I wanted to ensure that a move (changing the backing data structure, setting the UI, and printing it out to the screen) could be done all in one method to ensure that a step wouldn't be forgotten
+def performMiniBoardMove(board, p1, p2, uiRows, bigBoardSpace, miniBoardSpace, currPlayer):
+    board.miniBoards[bigBoardSpace][miniBoardSpace] = currPlayer.moveValue
+    setBoardUI(board, p1, p2, uiRows, bigBoardSpace)
+    drawBoard(uiRows)
 
+def performBigBoardMove(board, p1, p2, uiRows, bigBoardSpace, currPlayer):
+    board.bigBoard[bigBoardSpace] = currPlayer.moveValue
+    setBoardUI(board, p1, p2, uiRows, bigBoardSpace)
+    drawBoard(uiRows)
 
 
 if __name__ == "__main__":
-    spaceIndex = 8
-    rowOffset, columnOffset = COORDINATE_ADJUSTMENTS[spaceIndex]
+    # spaceIndex = 8
+    # rowOffset, columnOffset = COORDINATE_ADJUSTMENTS[spaceIndex]
+    board = Board()
     uiRows = initializeEmptyBoard()
-    setXBoard(uiRows, rowOffset, columnOffset)
+    # setXBoard(uiRows, rowOffset, columnOffset)
 
-    spaceIndex = 7
-    rowOffset, columnOffset = COORDINATE_ADJUSTMENTS[spaceIndex]
+    # spaceIndex = 7
+    # rowOffset, columnOffset = COORDINATE_ADJUSTMENTS[spaceIndex]
 
-    setOBoard(uiRows, rowOffset, columnOffset)
+    # setOBoard(uiRows, rowOffset, columnOffset)
 
-    p1 = Player(None, "", "x", True)
-    p2 = Player(None, "", "o", False)
-    m = [0,1,4,0,1,4,0,1,4]
-    spaceIndex = 6
-    rowOffset, columnOffset = COORDINATE_ADJUSTMENTS[spaceIndex]
-    setUnfinishedBoard(uiRows, rowOffset, columnOffset, m, p1, p2)
+    p1 = Player(setXBoard, "", "x", True)
+    p2 = Player(setOBoard, "", "o", False)
+    currPlayer = p2
+    bigBoardSpace = 8
 
-    drawBoard(uiRows)
+    performBigBoardMove(board, p1, p2, uiRows, bigBoardSpace, currPlayer)
+
+    miniBoardSpace = 4
+    bigBoardSpace = 2
+    performMiniBoardMove(board, p1, p2, uiRows, bigBoardSpace, miniBoardSpace, currPlayer)
+
+    # m = [0,1,4,0,1,4,0,1,4]
+    # spaceIndex = 6
+    # rowOffset, columnOffset = COORDINATE_ADJUSTMENTS[spaceIndex]
+    # setUnfinishedBoard(uiRows, rowOffset, columnOffset, m, p1, p2)
+
+    # drawBoard(uiRows)
 
 
 
